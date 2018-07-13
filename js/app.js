7562ba9eda4ecc2c7d7792c119db66052eba5c0c
tlacolley <thomaslacolley@gmail.com>
2018-07-13 10:15:49 +0200

    moegpoerjoer

--- js/app.js
+++ js/app.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
         dataType: "JSON",
 
        success : function(data, statut){ // code_html contient le HTML renvoyé
-           console.log(data.records);
+
            var mymap = L.map('mapid').setView([	46.467837, 2.136167], 6);
-           // var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
 
            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
             {
@@ -23,19 +23,53 @@ $(document).ready(function(){
 
 
                L.geoJSON(data.records[i].fields.geo_shape).addTo(mymap);
+               // console.log(data.records[i].fields.nom_chf.toUpperCase());
+               $.ajax({
+                url: `http://api.openweathermap.org/data/2.5/weather?q=${data.records[i].fields.nom_chf },fr&APPID=34bd5c6d73b8bf42222a0d0a58dbf160`,
+                type: 'GET', // Le type de la requête HTTP.
+                dataType: "JSON",
+
+                //${data.records[i].fields.nom_chf.toUpperCase() }
+
+                // error : function(resultat, statut, erreur){
+                //     console.log(resultat);
+                // },
+
+                 success : function(data){
+                     // for (i of data) {
+                     //
+                     //     console.log("data")
+                     //
+                     // }
+                     //
+                     // var marker = L.marker([data.coord]).addTo(mymap);
+                     // var popup = L.pop()
+                     // .setLatLng(data.coord)
+                     // .setContent(data.weather[0].main)
+                     // .openOn(map)
 
 
 
+                     // function onMapClick(e) {
+                     //     alert("You clicked the map at " + e.latlng);
+                     // }
+                     //
+                     // mymap.on('click', onMapClick);
+                     // var popup = L.popup();
+                     //
+                     // function onMapClick(e) {
+                     //     popup
+                     //     .setLatLng(e.latlng)
+                     //     .setContent("You clicked the map at " + e.latlng.toString())
+                     //     .openOn(mymap);
+                     // }
+
+                     // mymap.on('click', onMapClick);
+                     }
+               })
 
            }
 
-
-           // var circle = L.circle([51.508, -0.11], {
-           //     color: 'red',
-           //     fillColor: '#f03',
-           //     fillOpacity: 0.5,
-           //     radius: 500
-           // }).addTo(mymap);
 
 
            // marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
@@ -46,37 +80,10 @@ $(document).ready(function(){
            // .setContent("I am a standalone popup.")
            // .openOn(mymap);
 
-           // function onMapClick(e) {
-           //     alert("You clicked the map at " + e.latlng);
-           // }
-
-           // mymap.on('click', onMapClick);
-           var popup = L.popup();
-
-           // function onMapClick(e) {
-           //     popup
-           //     .setLatLng(e.latlng)
-           //     .setContent("You clicked the map at " + e.latlng.toString())
-           //     .openOn(mymap);
-           // }
-
-           // mymap.on('click', onMapClick);
-       
-           for (var i = 0; i < data.records.length; i++) {
-               $.ajax({
-                   url: `api.openweathermap.org/data/2.5/forecast?q=${data.records[i].fields.nom_chf},fr&APPID=34bd5c6d73b8bf42222a0d0a58dbf160`,
-                type: 'GET', // Le type de la requête HTTP.
-                dataType: "JSON",
-               })
 
 
-
-               console.log(data.records[i].fields.nom_chf)
-           }
 
         }
-       
-
 
 
 
